d0f8c44ed5a6fa8e0967aac43986dd90c06e8ba9
Will Dickson <wbd@caltech.edu>
2008-12-08 18:04:40 -0800

    Fixed some problems in the documentation.

--- realtime/include/motor_ctl.h
+++ realtime/include/motor_ctl.h
@@ -9,18 +9,18 @@
   
   This file is part of borf_control.
 
-  simple_step is free software: you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as published
-  by the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  borf_control is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
     
-  simple_step is distributed in the hope that it will be useful, but
+  borf_control is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public
-  License along with simple_step.  If not, see
+  License along with borf_control.  If not, see
   <http://www.gnu.org/licenses/>.
 
  -------------------------------------------------------------------------
